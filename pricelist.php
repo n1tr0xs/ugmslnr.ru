2c1ae65c87cc50980f236ed59de2d96e4078126b
n1tr0xs <niko4pok@gmail.com>
2023-07-04 11:12:57 +0300

    pricelist_page_generator.py core completed

--- pricelist.php
+++ pricelist.php
@@ -1,127 +1,363 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="/css/water.css">
-    <link rel="stylesheet" href="/css/menu.css">
-    <link rel="stylesheet" href="/css/table-counter.css">
-    <meta charset="utf-8">
-    <title></title>
-</head>
-<body>
-  <? include 'header.html'; ?>
-  <section>
-    <table class='fixed-head counter'>
-      <caption> Перечь платных услуг (работ) и их стоимости на гидрометеорологическую продукцию и информацию о состоянии окружающей среды, ее загрязнее на 2023 год </caption>
-      <thead>
-        <tr>
-          <td>№ п/п</td>
-          <td>Наименование</td>
-          <td>Цена за единицу продукции, без НДС, руб.</td>
-          <td>Примечание</td>
-        </tr>
-      </thead>
-<!--
+
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <link rel="stylesheet" href="/css/water.css">
+        <link rel="stylesheet" href="/css/menu.css">
+        <link rel="stylesheet" href="/css/table-counter.css">
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+      <? include 'header.html'; ?>
+      <section>
+        <table class='fixed-head counter'>
+          <caption> Перечь платных услуг (работ) и их стоимости на гидрометеорологическую продукцию и информацию о состоянии окружающей среды, ее загрязнее на 2023 год </caption>
+          <thead>
+            <tr>
+              <td>№ п/п</td>
+              <td>Наименование</td>
+              <td>Цена за единицу продукции, без НДС, руб.</td>
+              <td>Примечание</td>
+            </tr>
+          </thead>
+    
 <tbody>
-<tr> <td colspan="4" class='level-1'> </td> </tr>
-<tr class='level-2'> <td colspan="4" class='level-2'> </td> </tr>
-<tr class='level-3'>
-  <td class='level-3'>  </td>
-  <td> </td>
-  <td> </td>
-  <td> </td>
-</tr>
-</tbody>
--->
-
-    <tbody>
-        <tr> <td colspan="4" class='level-1'> Фактическая, в т.ч. </td> </tr>
-                <tr class='level-2'> <td colspan="4" class='level-2'> Температура воздуха </td> </tr>
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Температура воздуха в срок/среднедневная температура </td>
-    <td> 74,14 </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Средняя температура воздуха за сутки </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Средняя температура воздуха за декаду </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Средняя температура воздуха за месяц </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Средняя температура воздуха за год </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Максимальная/минимальная температура воздуха за сутки </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Максимальная/минимальная температура воздуха за декаду </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Максимальная/минимальная температура воздуха за месяц </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Средняя максимальная/минимальная температура воздуха за месяц </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Максимальная/минимальная температура воздуха за год </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Средняя максимальная/минимальная температура воздуха за год  </td>
-    <td>  </td>
-    <td> </td>
-</tr>
-
-<tr class='level-3'>
-    <td class='level-3'>  </td>
-    <td> Число дней с температурой выше/ниже заданной градации за год </td>
-    <td>  </td>
-    <td> </td>
-</tr>
+    <tr>
+        <td colspan="4" class='level-1'> Фактическая, в т.ч. оперативная (текущая) метеорологическая информация по запросу Заказчика - 1 пункт наблюдения, 1 срок, 1 элемент </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'> Температура воздуха </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Температура воздуха в срок/среднедневная температура </td>
+        <td> 74,14 </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура воздуха за сутки </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура воздуха за декаду </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура воздуха за месяц </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура воздуха за год </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная температура воздуха за сутки </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная температура воздуха за декаду </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная температура воздуха за месяц </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя максимальная/минимальная температура воздуха за месяц </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная температура воздуха за год </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя максимальная/минимальная температура воздуха за год  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Число дней с температурой выше/ниже заданной градации за год </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'> Температура почвы </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная температура на поверхности почвы в срок </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура на поверхности почвы за сутки </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура на поверхности почвы за декаду </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура на поверхности почвы за месяц </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Средняя температура на поверхности почвы за год </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная на поверхности почвы за сутки </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная на поверхности почвы за декаду </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная на поверхности почвы за месяц </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Максимальная/минимальная на поверхности почвы за год </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td> Число дней с температурой 0&deg; и ниже на поверхности почвы за год </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'> Влажность воздуха </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-3'>
+        <td class='level-3'>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                
+    <tr class='level-2'>
+        <td colspan="4" class='level-2'>  </td>
+    </tr>
+                </tbody>
+<tbody>
+    <tr>
+        <td colspan="4" class='level-1'> Расчетные метеорологические и климатические характиеристики за многолетний период </td>
+    </tr>
+                </tbody>
+<tbody>
+    <tr>
+        <td colspan="4" class='level-1'> Оператвно-прогностичестическая информация </td>
+    </tr>
+                </tbody>
+<tbody>
+    <tr>
+        <td colspan="4" class='level-1'> Гидрологическая информация </td>
+    </tr>
+                </tbody>
+<tbody>
+    <tr>
+        <td colspan="4" class='level-1'> Агронометрическая информация </td>
+    </tr>
+                </tbody>
+<tbody>
+    <tr>
+        <td colspan="4" class='level-1'> Информаия о загрязнении окружающей среды </td>
+    </tr>
+                </tbody>
+<tbody>
+    <tr>
+        <td colspan="4" class='level-1'> Прочие услуги </td>
+    </tr>
                 </tbody>
     </table>
-  </section>
-  <? include 'footer.html'; ?>
+</section>
+<? include 'footer.html'; ?>
 </body>
 </html>
+
+    
