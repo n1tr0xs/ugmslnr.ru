b7cc4eddedcf0d4e4e5517360e7ca6fe4fed52bd
n1tr0xs <m%73N2$?*c>
2023-07-05 12:54:10 +0300

    mobile footer support

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class='pc'>
   <link rel="stylesheet" href="/css/footer.css">
   <div id='row1'>
     <div id='company'>
@@ -9,15 +9,15 @@
       <p> Сообщить об ошибке: </p>
       <p> <a href='mailto:@mail.ru'> @mail.ru </a> </p>
     </div>
-    <div class='muted text-right'>
-      <p> Последнее обновление страницы: </p>
-      <p> <?=date("Y-m-d H:i:s", filemtime(basename($_SERVER['SCRIPT_FILENAME'])));?> </p>
-    </div>
   </div>
   <div id='row2'>
     <div>
       <p>©2023. Все права защищены.</p>
       <p class='muted'>При использовании материалов сайта ссылка обязательна.</p>
+    </div>
+    <div class='muted'>
+      <p> Последнее обновление страницы: </p>
+      <p> <?=date("Y-m-d H:i:s", filemtime(basename($_SERVER['SCRIPT_FILENAME'])));?> </p>
     </div>
   </div>
 </footer>
