aaab304e0640c5acf11e3e24a7356e0b2b452cde
n1tr0xs <m%73N2$?*c>
2023-07-05 12:24:55 +0300

    mobile menu support

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <header>
   <link rel="stylesheet" href="/css/menu.css">
-  <nav>
+  <script src="js/menu.js" charset="utf-8"></script>
+  <nav class='pc'>
     <ul class='navbar'>
-
       <li class='navbar'><a href='/'>Начальная страница</a></li>
-
       <li class='navbar dropdown'>
         <a class='dropdown' href='about.php'>О нас</a>
         <div class='navbar dropdown-content'>
@@ -14,19 +13,41 @@
           <a href='requisites.php'>Реквизиты</a>
         </div>
       </li>
-
       <li class='navbar'><a href='contacts.php'>Контакты</a></li>
-
       <li class='navbar dropdown'>
         <a class='dropdown' href='to_customers.php'>Потребителям</a>
-        <ul class='navbar dropdown-content'>
-          <li><a href='to_customers.php'>Потребителям</a></li>
+        <div class='navbar dropdown-content'>
+          <a href='to_customers.php'>Потребителям</a>
+          <a href="pricelist.php">Услуги</a>
+          <a href="office_hours.php">Часы приема</a>
+        </div>
+      </li>
+      <li class='navbar'><a href='weater_report.php'> Погода </a></li>
+    </ul>
+  </nav>
+  <nav class='mobile'>
+    <div class='menu-btn'>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="menu">
+      <ul>
+        <li><a href='/'>Начальная страница</a></li>
+        <li><a href='about.php'>О нас</a></li>
+        <ul>
+          <li><a href='leadership.php'>Руководство</a></li>
+          <li><a href='structure.php'>Структура</a></li>
+          <li><a href='requisites.php'>Реквизиты</a></li>
+        </ul>
+        <li><a href='contacts.php'>Контакты</a></li>
+        <li><a href='to_customers.php'>Потребителям</a></li>
+        <ul>
           <li><a href="pricelist.php">Услуги</a></li>
           <li><a href="office_hours.php">Часы приема</a></li>
         </ul>
-      </li>
-
-      <li class='navbar'><a href='weater_report.php'> Погода </a></li>
-    </ul>
+        <li><a href='weater_report.php'> Погода </a></li>
+      </ul>
+    </div>
   </nav>
 </header>
---
+++ news/short_news/short_news5.php
@@ -0,0 +1,9 @@
+<div class='news'>
+  <h2> News 5 </h2>
+  <div>
+    <p> Paragraph 1 </p>
+    <p> Paragraph 2 </p>
+    <p> Paragraph 3 </p>
+    <p> Paragraph 4 </p>
+  </div>
+</div>
