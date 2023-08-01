33471c7e54dc6b186b344b7808b543fbed5e7b96
n1tr0xs <m%73N2$?*c>
2023-08-01 14:10:00 +0300

    office hours example

--- to_customers/office_hours.php
+++ to_customers/office_hours.php
@@ -8,10 +8,15 @@
 <body>
   <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ; ?>
   <div id='containter'>
-  <div id='content'>
-
-  </div>
-  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php';  ?>
+    <div id='content'>
+      <p>
+        Понедельник - Пятница: 8:00 - 17:00.
+      </p>
+      <p>
+        Суббота - Воскресенье: выходной.
+      </p>
+    </div>
+    <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php';  ?>
     <span style="display: block; clear: both;"></span>
   </div>
   <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';  ?>
