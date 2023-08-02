86825109a7ecd1601b3d2a1cb7ee723b0aae1577
n1tr0xs <m%73N2$?*c>
2023-08-02 13:58:03 +0300

    leadership information

--- about/leadership.php
+++ about/leadership.php
@@ -4,32 +4,56 @@
   <meta charset="utf-8">
   <link rel="stylesheet" href="/css/master.css">
   <title>Руководство</title>
+  <style>
+    .leader {
+      display:  flex;
+      align-items: center;
+      justify-content: left;
+    }
+    .leader > div > p:nth-child(2) {
+      font-style: italic;
+    }
+  </style>
 </head>
 <body>
   <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
   <div id='containter'>
     <div id='content'>
-      <h3> Руководство </h3>
+      <h3> Руководство УГМС по ЛНР</h3>
 
-      <hr>
       <div class='leader'>
-        <figure>
-          <img src="files/t.jpg" alt="" sizes="(max-width: 734px) 100vw, 734px">
-        </figure>
+        <img src="/files/imgs/photo-w.png">
         <div>
-          <p>ФИО</p>
-          <p>ОТДЕЛ</p>
+          <p class='name'>Баева Ирина Николаевна</p>
+          <p>Начальник Учреждения</p>
+          <p>Электронная почта: <a href='mailto:ugms.lnr@mail.ru'>ugms.lnr@mail.ru</a></p>
         </div>
       </div>
 
-      <hr>
       <div class='leader'>
-        <figure>
-          <img src="files/t.jpg" alt="" sizes="(max-width: 734px) 100vw, 734px">
-        </figure>
+        <img src="/files/imgs/photo-w.png">
         <div>
-          <p>ФИО</p>
-          <p>ОТДЕЛ</p>
+          <p class='name'>Шишова Светлана Сергеевна</p>
+          <p>Заместитель начальника Учреждения – начальник центра мониторинга загрязнения окружающей среды</p>
+          <p>Электронная почта: <a href='mailto:ugms.lnr@mail.ru'>ugms.lnr@mail.ru</a></p>
+        </div>
+      </div>
+
+      <div class='leader'>
+        <img src="/files/imgs/photo-w.png">
+        <div>
+          <p class='name'>Шубенок Светлана Николаевна</p>
+          <p>Заместитель начальника Учреждения – начальник Гидрометцентра Луганской Народной Республики</p>
+          <p>Электронная почта: <a href='mailto:ugms.lnr@mail.ru'>ugms.lnr@mail.ru</a></p>
+        </div>
+      </div>
+
+      <div class='leader'>
+        <img src="/files/imgs/photo-w.png">
+        <div>
+          <p class='name'>Грановская Галина Владимировна</p>
+          <p>Главный бухгалтер – начальник отдела бухгалтерского учета</p>
+          <p>Электронная почта: <a href='mailto:ugms.lnr@mail.ru'>ugms.lnr@mail.ru</a></p>
         </div>
       </div>
 
