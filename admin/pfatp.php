b803ccbaa16c9e3e779fe59655f2d5c3daf7198f
n1tr0xs <m%73N2$?*c>
2023-08-04 13:19:10 +0300

    pfatp admin panel

---
+++ admin/pfatp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/master.css">
+  <title>Администрирование - вероятностный прогноз температуры воздуха и осадков</title>
+</head>
+<body>
+  <div id='containter'>
+    <div id='content'>
+      <form action="/admin/pfatp_exec.php" method="post">
+        <input type="text" name="date" placeholder="Дата"><br>
+        <input type="text" name="desc" placeholder="Описание"><br>
+        <button>Отправить данные</button>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
---
+++ admin/pfatp_exec.php
@@ -0,0 +1,18 @@
+<?
+$date = $_POST['date'];
+$desc = $_POST['desc'];
+
+$text = '';
+
+$text .= "<p class='day'>Прогноз на ". $date. "</p>";
+$text .= "<p class='description'>Прогноз на ". $desc. "</p>";
+
+$filepath = $_SERVER['DOCUMENT_ROOT'] . '/updatable/pfatp.html';
+$file = fopen($filepath, 'w');
+fwrite($file, $text);
+fclose($file);
+
+echo "Данные отправлены.<br>";
+echo "<a href='/admin/pfatp.php'>Страница администрирования</a><br>";
+echo "<a href='/index.php'>Главная страница сайта</a><br>";
+?>
