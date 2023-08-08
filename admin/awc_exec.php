379c27d6a9d35358d9c22d5ed231fec5c5e181a8
n1tr0xs <m%73N2$?*c>
2023-08-08 10:05:37 +0300

    uncommented auth

--- admin/awc_exec.php
+++ admin/awc_exec.php
@@ -5,10 +5,10 @@ $_file = basename(__FILE__, '_exec.php');
 
 $input_password = hash('sha256', $_POST['password']);
 $check_string = file_get_contents($_SERVER['DOCUMENT_ROOT']. "/pwd/{$_file}");
-// if($input_password != $check_string){
-// 	echo "Неверный пароль!";
-// 	exit();
-// }
+if($input_password != $check_string){
+	echo "Неверный пароль!";
+	exit();
+}
 
 
 
