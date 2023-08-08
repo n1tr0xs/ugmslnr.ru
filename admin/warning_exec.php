30c4b69dc39dd38177b922b7441adc4dfb8d4f0d
n1tr0xs <m%73N2$?*c>
2023-08-08 09:01:22 +0300

    text changed

--- admin/warning_exec.php
+++ admin/warning_exec.php
@@ -6,9 +6,9 @@ $pollution = $_POST['pollution'];
 
 $text = "<div id='warnings' class='text-left'>";
 
-$text .= "<div class='warning". ($meteo ? " warned" : ""). "'><div><div class='circle'></div>Метеорологическое</div><span class='warning-text'></span></div>";
-$text .= "<div class='warning". ($agro ? " warned" : ""). "'><div><div class='circle'></div>Гидрометеорологическое</div><span class='warning-text'></span></div>";
-$text .= "<div class='warning". ($hydro ? " warned" : ""). "'><div><div class='circle'></div>Агрометеорологическое</div><span class='warning-text'></span></div>";
+$text .= "<div class='warning". ($meteo ? " warned" : ""). "'><div><div class='circle'></div>Метеорология</div><span class='warning-text'></span></div>";
+$text .= "<div class='warning". ($agro ? " warned" : ""). "'><div><div class='circle'></div>Гидрология</div><span class='warning-text'></span></div>";
+$text .= "<div class='warning". ($hydro ? " warned" : ""). "'><div><div class='circle'></div>Агрометеорология</div><span class='warning-text'></span></div>";
 $text .= "<div class='warning". ($pollution ? " warned" : ""). "'><div><div class='circle'></div>Загрязнение окружающей среды</div><span class='warning-text'></span></div>";
 $text .= "</div>";
 
