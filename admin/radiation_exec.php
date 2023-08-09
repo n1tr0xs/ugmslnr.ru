bb6cdd7fc80dead3142fb4a94f54600c1705764a
n1tr0xs <m%73N2$?*c>
2023-08-09 10:56:30 +0300

    radiation admin panel added

---
+++ admin/radiation_exec.php
@@ -0,0 +1,23 @@
+<?
+$_file = basename(__FILE__, '_exec.php');
+
+$input_password = hash('sha256', $_POST['password']);
+$check_string = file_get_contents($_SERVER['DOCUMENT_ROOT']. "/pwd/{$_file}");
+if($input_password != $check_string){
+	echo "Неверный пароль!";
+	exit();
+}
+$date = $_POST['date'];
+$desc = $_POST['desc'];
+$text = "";
+$text .= "Уровень радиации на ". $date. " по территории Луганской Народной Республики составил ". $desc;
+
+$filepath = $_SERVER['DOCUMENT_ROOT'] . "/updatable/{$_file}.html";
+$file = fopen($filepath, 'w');
+fwrite($file, $text);
+fclose($file);
+
+echo "Данные отправлены.<br>";
+echo "<a href='/admin/{$_file}.html'>Страница администрирования</a><br>";
+echo "<a href='/index.html'>Главная страница сайта</a><br>";
+?>
