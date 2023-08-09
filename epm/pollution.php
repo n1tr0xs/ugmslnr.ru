519898ae0d0ff5484d2f345e3576f804de089c45
n1tr0xs <m%73N2$?*c>
2023-08-09 11:20:40 +0300

    polution -> pollution

--- epm/pollution.php
+++ epm/pollution.php
@@ -10,7 +10,7 @@
   <div id='containter'>
     <div id='content'>
       <h3> Загрязнение атмосферного воздуха</h3>
-      <? include $_SERVER['DOCUMENT_ROOT'] . '/updatable/polution.html';  ?>
+      <? include $_SERVER['DOCUMENT_ROOT'] . '/updatable/pollution.html';  ?>
     </div>
     <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php';  ?>
     <span style="display: block; clear: both;"></span>
