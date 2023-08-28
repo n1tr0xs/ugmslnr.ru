3c00c3e087ecfa92d196e5648a496b4a53742254
n1tr0xs <niko4pok@gmail..com>
2023-08-28 14:13:49 +0300

    weather forecast text now uses replace into sql statement instead of multiple sql and php checks

--- admin/weather_forecast_text_exec.php
+++ admin/weather_forecast_text_exec.php
@@ -8,20 +8,26 @@ $date = $_POST['date'];
 $desc_city = $_POST['desc_city'];
 $desc_region = $_POST['desc_region'];
 
-$sql = "select count(*) c from `ugmslnr`.`weather_forecast_text` where `date`='{$date}'";
-$row = get_row($conn, $sql);
-if($row['c']){
-  $sql = "
-  update `ugmslnr`.`weather_forecast_text` 
-  set `desc_city`='{$desc_city}', `desc_region`='{$desc_region}'
-  where `date`='{$date}'
-  ";
-} else {
-  $sql = "
-  insert into `ugmslnr`.`weather_forecast_text` 
-  values (NULL, '{$date}', '{$desc_city}', '{$desc_region}');
-  ";
-}
+// $sql = "select count(*) c from `ugmslnr`.`weather_forecast_text` where `date`='{$date}'";
+// $row = get_row($conn, $sql);
+// if($row['c']){
+//   $sql = "
+//   update `ugmslnr`.`weather_forecast_text` 
+//   set `desc_city`='{$desc_city}', `desc_region`='{$desc_region}'
+//   where `date`='{$date}'
+//   ";
+// } else {
+//   $sql = "
+//   insert into `ugmslnr`.`weather_forecast_text` 
+//   values (NULL, '{$date}', '{$desc_city}', '{$desc_region}');
+//   ";
+// }
+
+$sql = "
+replace into `ugmslnr`.`weather_forecast_text` values 
+('{$date}', '{$desc_city}', '{$desc_region}')
+";
+
 if($conn->query($sql) === TRUE){
 	echo "Данные отправлены.<br>";
 } else {
