64a66372d184afe9e3a69c51a328ed8f809d2e7d
n1tr0xs <niko4pok@gmail..com>
2023-08-28 15:31:48 +0300

    info page added

---
+++ hydromet/hydro_review.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/master.css">
+  <title>Отдел гидрологии</title>
+</head>
+<body>
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
+  <div id='containter'>
+    <div id='content'>
+      <!-- TODO: auto list -->
+      <tr><td><a href="/updatable/hydro_review/06.2023.pdf">Гидрологический обзор за июнь 2023 года</a></td></tr>
+    </div>
+    <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php'; ?>
+    <span style="display: block; clear: both;"></span>
+  </div>
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
+</body>
+</html>
