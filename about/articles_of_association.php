4771558557b1240b337c755e9847ba35502d1af0
n1tr0xs <niko4pok@gmail..com>
2023-08-28 15:22:16 +0300

    info files added

--- about/articles_of_association.php
+++ about/articles_of_association.php
@@ -11,10 +11,10 @@
     <div id='content'>
       <h3> Уставные документы </h3>
       <ul class='no-bullets'>
-        <li><a href="/files/documents/">Устав ФГБУ "УГМС по ЛНР"</a></li>
-        <li><a href="/files/documents/">Приказ</a></li>
-        <li><a href="/files/documents/">Выписка из Единого государственного реестра юридических лиц</a></li>
-        <li><a href="/files/documents/">Свидетельство о государственной регистрации юридичческого лица (ИНН)</a></li>
+        <li><a href="/files/documents/regulation.pdf">Устав ФГБУ "УГМС по ЛНР"</a></li>
+        <li><a href="/files/documents/oerder.pdf">Приказ</a></li>
+        <li><a href="/files/documents/ul-1239400002610-20230828151953.pdf">Выписка из Единого государственного реестра юридических лиц</a></li>
+        <li><a href="/files/documents/">Свидетельство о государственной регистрации юридического лица (ИНН)</a></li>
       </ul>
     </div>
     <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php'; ?>
