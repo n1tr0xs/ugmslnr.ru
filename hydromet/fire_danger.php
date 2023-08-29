2188d37ed31286d5ccb5c32dae0fd255785934d8
n1tr0xs <niko4pok@gmail..com>
2023-08-29 11:36:40 +0300

    fire danger forecast now on separate page

--- hydromet/fire_danger.php
+++ hydromet/fire_danger.php
@@ -50,10 +50,7 @@
           </tr>
         </tbody>
       </table>
-      <div class='text-center'>
-        <img src="/updatable/fire_danger.png" alt="Пожарная опасность" class='no-border'>
-      </div>
-      <? include $_SERVER['DOCUMENT_ROOT'] . '/updatable/fire_forecast.html'; ?>
+      <p><a href="/hydromet/fire_danger_forecast.php">Прогноз пожароопасности</a></p>
     </div>
     <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php'; ?>
     <span style="display: block; clear: both;"></span>
