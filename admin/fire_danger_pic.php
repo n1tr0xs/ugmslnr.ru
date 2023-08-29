b14a5c4c0eb550cbc8772a025d4fbad84e49a652
n1tr0xs <niko4pok@gmail..com>
2023-08-29 13:47:46 +0300

    file drop area styling

--- admin/fire_danger_pic.php
+++ admin/fire_danger_pic.php
@@ -10,7 +10,11 @@
   <div id='containter'>
     <div id='content'>
       <form action="/admin/fire_danger_pic_exec.php" method="post" enctype="multipart/form-data">
-        <div class='form-row'><input type="file" name="fileToUpload"></div>
+        <div class='form-row file-drop-area'>
+          <span class="fake-btn">Выберите файл</span>
+          <span class="file-msg">или перетащите файл сюда</span>
+          <input type="file" name="fileToUpload" class="file-input">
+        </div>
         <div class='form-row'><button type="submit">Отправить данные</button></div>
       </form><br> <a href='/admin/'> Список панелей администрирования </a>
     </div>
