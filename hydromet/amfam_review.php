e0af3826ad2e4125f948f26d30cec18d6c736798
n1tr0xs <niko4pok@gmail..com>
2023-08-29 12:55:05 +0300

    info files added; amfam reviews page added"

---
+++ hydromet/amfam_review.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/master.css">
+  <title>Отдел агрометеорологических прогнозов и агрометеорологии</title>
+</head>
+<body>
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
+  <div id='containter'>
+    <div id='content'>
+      <table class='no-border text-left table-striped'>
+        <?
+        // reads files from folder and creates list of links
+        $folder = "amfam_review/";
+        $dir = $_SERVER['DOCUMENT_ROOT'] . "/updatable/". $folder;
+        if (is_dir($dir)) {
+          if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+              if(strpos($file, ".pdf") !== false){
+                ?>
+                <tr><td><a href="/updatable/<? echo $folder.$file; ?>">  <? echo $file; ?></a></td></tr>
+                <?
+              }
+            }
+            closedir($dh);
+          }
+        }
+        ?>
+      </table>
+    </div>
+    <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php'; ?>
+    <span style="display: block; clear: both;"></span>
+  </div>
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
+</body>
+</html>
