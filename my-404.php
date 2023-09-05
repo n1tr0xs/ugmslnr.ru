5bc2ddeb0db5f956ed87ccbf8476e449f9568683
n1tr0xs <niko4pok@gmail..com>
2023-09-05 09:51:52 +0300

    custom 404 page created

---
+++ my-404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/styles.html'; ?>
+  <title>Страница не найдена</title>
+</head>
+<body>
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php'; ?>
+  <div id='containter'>
+    <div id='content'>
+      <h3>Страница не найдена</h3>
+      <p><a href="/">Вернуться на главную</a></p>
+    </div>
+    <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/aside.php'; ?>
+    <span style="display: block; clear: both;"></span>
+  </div>
+  <? include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
+</body>
+</html>
