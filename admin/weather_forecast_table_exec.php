17813b215aa4ae3d706f68ed6307919f806862c1
n1tr0xs <niko4pok@gmail..com>
2023-11-02 13:28:13 +0300

    night icons added

--- admin/weather_forecast_table_exec.php
+++ admin/weather_forecast_table_exec.php
@@ -6,6 +6,10 @@ $conn = connect("editor", $password);
 $date = $conn->real_escape_string($_POST['date']);
 $day_part = $conn->real_escape_string($_POST['day_part']);
 $icon = $conn->real_escape_string($_POST['icon']);
+if($day_part == 1){
+	if($icon == 1) $icon = 12;
+	if($icon == 7) $icon = 13;
+}
 $wind_direction = $conn->real_escape_string($_POST['wind_direction']);
 $wind_speed = $conn->real_escape_string($_POST['wind_speed']);
 $temperature = $conn->real_escape_string($_POST['temperature']);
