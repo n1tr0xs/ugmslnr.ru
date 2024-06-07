6cc19372843de34fbc2bb897854eade328a3d97e
Programmist <niko4pok@gmail.com>
2024-06-07 09:17:31 +0300

    content upload

--- press/template.php
+++ press/template.php
@@ -2,15 +2,15 @@
 <html lang="en" dir="ltr">
 <head>
   <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html'; ?>
-  <meta name="description" content="">
-  <title></title>
+  <meta name="description" content="<?=$news_title;?>">
+  <title><?=$news_title;?></title>
 </head>
 <body>
   <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
   <div id='containter'>
     <div id='content'>
-    <!---->
-    <!---->
+        <h3><?=$news_title;?></h3>
+    <--CONTENT-->
     </div>
     <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
     </div>
