099da364fe8b00750ba8debf5ee481e45577452e
Programmist <niko4pok@gmail.com>
2024-06-13 10:30:16 +0300

    content update

--- admin/add_news_assets_exec.php
+++ admin/add_news_assets_exec.php
@@ -2,23 +2,27 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/requires/funcs.php';
 
 $news_name = $_POST['news-name'];
+$files = $_FILES['files'];
+
 $path = $_SERVER['DOCUMENT_ROOT'] . "/press/assets/". $news_name;
 
-$files = $_FILES['files'];
 $tmp_dest = array();
 $max_i = count($files['name']);
-$result = TRUE;
+$result = FALSE;
 
 for($i=0; $i<$max_i; ++$i){
     $tmp = $files['tmp_name'][$i];
     mkdir($path);
     $dest = $path . "/" . $files['name'][$i];
-    $result = move_uploaded_file($tmp, $dest) && $result;
+    echo "Загрузка файла ". $files['name'][$i]. "...<br>";
+    if(move_uploaded_file($tmp, $dest) === TRUE)
+        echo "Файл ". $files['name'][$i]. " загружен.";
+    else
+        echo "Файл ". $files['name'][$i]. " не загружен.";
+    echo "<br>";
 }
 
 // $dest = $_SERVER['DOCUMENT_ROOT'] . "/press/" . $_FILES['files']['name'];
 // $result = move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $dest);
 
-if($result == TRUE)
-  echo "Данные отправлены.";
 echo "<br><a href='/admin/". basename(__FILE__, '_exec.php'). ".php'>Вернуться на страницу ввода </a>";
---
+++ press/Aktsiya-Okna-Rossii-2024-goda.php
@@ -0,0 +1,30 @@
+<? $news_title="Акция Окна России 2024 года"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html'; ?>
+  <meta name="description" content="<?=$news_title;?>">
+  <title><?=$news_title;?></title>
+</head>
+<body>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
+  <div id='containter'>
+    <div id='content'>
+        <img src="thumb.jpg" style="display: none">
+        <h3><?=$news_title;?></h3>
+        <p> В федеральном государственном бюджетном учреждении «Управление по гидрометеорологии и мониторингу окружающей среды по Луганской Народной Республике» проведена акция «Окна России», приуроченная к празднованию Дня России. </p>
+        <div class='img-carousel' style="height: 40em;">
+            <img src="0.jpg" class="asset h50"/>
+            <img src="1.jpg" class="asset h50"/>
+            <img src="2.jpg" class="asset h50"/>
+            <img src="3.jpg" class="asset h50"/>
+            <img src="4.jpg" class="asset h50"/>
+            <img src="5.jpg" class="asset h50"/>
+        </div>
+    </div>
+    <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
+    </div>
+  </div>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/footer.php'; ?>
+</body>
+</html>
