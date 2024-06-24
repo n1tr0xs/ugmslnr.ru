dcb07b9f669f0ee2c0f390792938abfcc5687aa4
Programmist <niko4pok@gmail.com>
2024-06-24 12:29:43 +0300

    sql admin; fix weather_forect_table

--- admin/weather_forecast/weather_forecast_table/exec.php
+++ admin/weather_forecast/weather_forecast_table/exec.php
@@ -9,9 +9,23 @@ $wind_direction = $conn->real_escape_string($_POST['wind_direction']);
 $wind_speed = $conn->real_escape_string($_POST['wind_speed']);
 $temperature = $conn->real_escape_string($_POST['temperature']);
 
+// echo "<p>DUMP: ";
+// var_dump($_POST);
+// echo "</p>";
+
+// echo "<p>Вы ввели: </p>";
+// echo "<p>Дата: {$date}</p>";
+// echo "<p>Часть дня: {$day_part}</p>";
+// echo "<p>Иконка: {$icon}</p>";
+// echo "<p>Направление ветра: {$wind_direction}</p>";
+// echo "<p>Скорость ветра: {$wind_speed}</p>";
+// echo "<p>Температура: {$temperature}</p>";
+
+
+
 $sql = "
 replace into `ugmslnr`.`weather_forecast_table` values 
-('{$date}', {$day_part}, '{$icon}', {$wind_direction}, '{$temperature}', '{$wind_speed}')
+('{$date}', '{$day_part}', '{$icon}', {$wind_direction}, '{$wind_speed}', '{$temperature}')
 ";
 
 exec_result($sql);
