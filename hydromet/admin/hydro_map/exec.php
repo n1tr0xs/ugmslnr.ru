0702e9c70e24a02b8fb80c381de1e91245ea9752
Programmist <niko4pok@gmail.com>
2025-02-12 15:30:52 +0300

    smth was broken

--- hydromet/admin/hydro_map/exec.php
+++
@@ -1,8 +0,0 @@
-<?
-require $_SERVER['DOCUMENT_ROOT'] . '/requires/funcs.php';
-
-$img_path = $_SERVER['DOCUMENT_ROOT'] . "/updatable/hydro_map.png";
-$result = move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $img_path);
-          
-echo "<p>Данные". ($result ? " " : " не "). "отправлены.</p>";
-echo "<p><a href='.'>Вернуться на страницу ввода </a></p>";
--- hydromet/admin/upload_monthly_statements/upload_clim_char/exec.php
+++
@@ -1,8 +0,0 @@
-<?
-require $_SERVER['DOCUMENT_ROOT'] . '/requires/funcs.php';
-
-$dest = $_SERVER['DOCUMENT_ROOT'] . "/updatable/clim_char/" . $_FILES['fileToUpload']['name'];
-$result = move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $dest);
-
-echo "<p>Данные". ($result ? " " : " не "). "отправлены.</p>";
-echo "<p><a href='.'>Вернуться на страницу ввода </a></p>";
