681b5da554940df8f28978918b0262e21d93f78a
Programmist <niko4pok@gmail.com>
2025-02-12 15:33:09 +0300

    Весеннее половодье

--- admin/high_water/current/exec.php
+++ admin/high_water/current/exec.php
@@ -2,13 +2,13 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/requires/funcs.php';
 $conn = connect("visiter", "visiter_ugms");
 
-$description = $conn->real_escape_string($_POST['description']);
 $start = $conn->real_escape_string($_POST['start']);
-$end = $conn->real_escape_string($_POST['end']);
+$description = $conn->real_escape_string($_POST['description']);
+$description = str_replace('ГП-I ', 'ГП-I&nbsp;', $description);
 
 $sql = "
 insert into `ugmslnr`.`high_water_current` values 
-(NULL, '{$start}', '{$end}', '{$description}')
+(NULL, '{$start}', '{$description}')
 ";
 
 exec_result($sql);
