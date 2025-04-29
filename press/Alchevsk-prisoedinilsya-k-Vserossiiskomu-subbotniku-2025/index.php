b3fcded3bdaccb0d51a371f6fd792229cf61c91d
Programmist <niko4pok@gmail.com>
2025-04-29 11:19:15 +0300

    news

---
+++ press/Alchevsk-prisoedinilsya-k-Vserossiiskomu-subbotniku-2025/index.php
@@ -0,0 +1,32 @@
+<? $news_title="Алчевск присоединился к Всероссийскому субботнику 2025"; ?>
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+<head>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html'; ?>
+  <meta name="description" content="<?=$news_title;?>">
+  <title><?=$news_title;?></title>
+  <link rel="canonical" href="<?
+    $uri = explode('/', $_SERVER['REQUEST_URI']);
+    $uri[count($uri)-1] = '';
+    echo 'https://'. $_SERVER['HTTP_HOST']. join('/', $uri);
+  ?>
+  "/>
+</head>
+<body>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
+  <div id='containter'>
+    <div id='content'>
+        <h3><?=$news_title;?></h3>
+    <p>В рамках федерального проекта «Формирование комфортной городской среды» национального проекта «Инфраструктура для жизни», а также в целях благоустройства и повышения качества городской среды, 25 апреля коллектив ФГБУ «УГМС по ЛНР» принял активное участие в проведении работ по наведению порядка на прилегающей территории в соответствии с нормами благоустройства. Проведена уборка листвы, сухих веток и мусора.</p>
+<div class='img-carousel' style='max-height: 43em'>
+	<img src='1.png'>
+	<img src='2.png'>
+	<img src='3.png'>
+</div>
+    </div>
+    <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
+    </div>
+  </div>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/footer.php'; ?>
+</body>
+</html>
---
+++ press/Kollektiv-FGBU-UGMS-po-LNR-prisoedinilsya-k-respublikanskoi-aktsii-CHistaya-Respublika--2025/index.php
@@ -0,0 +1,34 @@
+<? $news_title="Коллектив ФГБУ «УГМС по ЛНР» присоединился к республиканской акции «Чистая Республика – 2025»"; ?>
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+<head>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html'; ?>
+  <meta name="description" content="<?=$news_title;?>">
+  <title><?=$news_title;?></title>
+  <link rel="canonical" href="<?
+    $uri = explode('/', $_SERVER['REQUEST_URI']);
+    $uri[count($uri)-1] = '';
+    echo 'https://'. $_SERVER['HTTP_HOST']. join('/', $uri);
+  ?>
+  "/>
+</head>
+<body>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
+  <div id='containter'>
+    <div id='content'>
+        <h3><?=$news_title;?></h3>
+    <p>16 апреля, согласно распоряжению Главы ЛНР «О проведении республиканской акции «Чистая Республика – 2025», в целях обеспечения экологически благоприятной среды для проживания населения на территории ЛНР, коллектив ФГБУ «УГМС по ЛНР» принял активное участие в проведении работ по наведению порядка на прилегающей территории в соответствии с нормами благоустройства.</p>
+<p>Проведена уборка старой листвы, сухих веток и мусора. Вырублен сухостой. Побелены стволы деревьев, оформлены клумбы и посажены цветы.</p>
+<p>Прекрасная погода и сплоченные действия коллектива учреждения позволили справиться с большим объемом работы быстро и качественно! Слаженная работа помогла навести чистоту и порядок после зимы!</p>
+<div class='img-carousel' style='max-height: 43em'>
+	<img src='1.png'>
+	<img src='2.png'>
+	<img src='3.png'>
+</div>
+    </div>
+    <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
+    </div>
+  </div>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/footer.php'; ?>
+</body>
+</html>
