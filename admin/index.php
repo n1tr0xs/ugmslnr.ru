71fbd5172ea93030747fd6cbc083b64582200436
Programmist <niko4pok@gmail.com>
2025-05-15 08:36:47 +0300

    #

--- admin/index.php
+++ admin/index.php
@@ -1,30 +1,33 @@
 <? $page_name = 'Панели администрирования'; ?>
 <!DOCTYPE html>
 <html lang="ru" dir="ltr">
+
 <head>
   <meta http-equiv="robots" content="noindex">
-  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html';?>
-  <title><?=$page_name;?></title>
+  <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html'; ?>
+  <title><?= $page_name; ?></title>
 </head>
+
 <body>
   <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
   <div id='containter'>
     <div id='content'>
-        <h3><?=$page_name;?></h3>
-        <div id='admin-panels' style="display: flex; flex-wrap: nowrap; flex-direction: column;">
-            <script>
-            document.addEventListener("DOMContentLoaded", (event) => {
-                output_admin_panels([<?
-                    foreach(array_diff(scandir('.'), array('..', '.')) as $name) 
-                        if(is_dir($name)) 
-                            echo "'{$name}/',\n";
-                ?>])
-            });
-            </script>
-        </div>
+      <h3><?= $page_name; ?></h3>
+      <div id='admin-panels' style="display: flex; flex-wrap: nowrap; flex-direction: column;">
+        <script>
+          document.addEventListener("DOMContentLoaded", (event) => {
+            output_admin_panels([<?
+            foreach (array_diff(scandir('.'), array('..', '.')) as $name)
+              if (is_dir($name))
+                echo "'{$name}/',\n";
+            ?>])
+          });
+        </script>
+      </div>
     </div>
     <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
   </div>
   <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/footer.php'; ?>
 </body>
+
 </html>
