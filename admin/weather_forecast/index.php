8ab26fb4873d8f5ac8407e21bf2239c78860f15b
Programmist <niko4pok@gmail.com>
2025-05-16 13:05:55 +0300

    fix: admin/weather_forecast/index

--- admin/weather_forecast/index.php
+++ admin/weather_forecast/index.php
@@ -4,25 +4,25 @@
 
 <head>
     <meta http-equiv="robots" content="noindex">
-    <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html';?>
-    <title><?=$page_name;?></title>
+    <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/head.html'; ?>
+    <title><?= $page_name; ?></title>
 </head>
 
 <body>
     <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
     <div id='containter'>
         <div id='content'>
-            <h3><?=$page_name;?></h3>
+            <h3><?= $page_name; ?></h3>
             <div id='admin-panels' style="display: flex; flex-wrap: nowrap; flex-direction: column;">
                 <script>
-                document.addEventListener("DOMContentLoaded", (event) => {
-                    output_admin_panels([ < ?
-                        foreach(array_diff(scandir('.'), array('..', '.')) as $name)
-                        if (is_dir($name))
-                            echo "'{$name}/',\n"; ?
-                        >
-                    ])
-                });
+                    document.addEventListener("DOMContentLoaded", (event) => {
+                        output_admin_panels([ <?
+                        foreach (array_diff(scandir('.'), array('..', '.')) as $name)
+                            if (is_dir($name))
+                                echo "'{$name}/',\n";
+                        ?>
+                        ])
+                    });
                 </script>
             </div>
         </div>
