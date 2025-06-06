193dbe01017406d2a220a64769c185ba9d64ab88
Programmist <niko4pok@gmail.com>
2025-06-06 09:16:55 +0300

    semantic

--- about/contacts.php
+++ about/contacts.php
@@ -11,19 +11,19 @@
     <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
     <div id='containter'>
         <div id='content'>
-            <h2> Общая контактная информация </h1>
-                <p>
-                    <b> Юридический адрес: </b>
-                    <span> 291008, ЛНР, г.о. город Луганск, г. Луганск, кв-л Аграрный, 5Б </span>
-                </p>
-                <p>
-                    <b> Электронная почта: </b>
-                    <a href='mailto:ugms@lnr.mecom.ru'>ugms@lnr.mecom.ru</a>
-                </p>
-                <p>
-                    <b> Телефон (городской): </b>
-                    <span>857-2-96-60-61</span>
-                </p>
+            <h2> Общая контактная информация </h2>
+            <div>
+                <span class="bold"> Юридический адрес: </span>
+                <span> 291008, ЛНР, г.о. город Луганск, г. Луганск, кв-л Аграрный, 5Б </span>
+            </div>
+            <div>
+                <span class="bold"> Электронная почта: </span>
+                <a href='mailto:ugms@lnr.mecom.ru'>ugms@lnr.mecom.ru</a>
+            </div>
+            <div>
+                <span class="bold"> Телефон (городской): </span>
+                <span>857-2-96-60-61</span>
+            </div>
         </div>
         <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
     </div>
--- to_customers/contacts.php
+++ to_customers/contacts.php
@@ -11,27 +11,37 @@
     <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/header.php'; ?>
     <div id='containter'>
         <div id='content'>
-            <p class="bold text-center">Отдел специализированного гидрометеорологического обеспечения</p>
-            <p class="bold text-center">(по работе с Заказчиками)</p>
-            <p>Наши контакты:</p>
-            <p>Адрес ФГБУ «УГМС по ЛНР»: 291008, ЛНР, г.о. город Луганск, г. Луганск, кв-л Аграрный, 5Б</p>
-            <p>Электронная почта: <a href='mailto:ugms@lnr.mecom.ru'>ugms@lnr.mecom.ru</a></p>
-            <p class="bold">Проезд:</p>
-            <ul class="no-bullets">
-                <li>от автовокзала автобус - №197, №250</li>
-                <li>от ж\д вокзала маршрутное такси - № 183</li>
-                <li>с восточных кварталов – № 183, №132А</li>
-                <li>с южных кварталов – № 141</li>
-            </ul>
-            <h4>Остановка «Луганский государственный аграрный университет»</h4>
+            <div class="bold text-center">
+                <p>Отдел специализированного гидрометеорологического обеспечения</p>
+                <p>(по работе с Заказчиками)</p>
+            </div>
+            <div>
+                <p>Наши контакты:</p>
+                <p>Адрес ФГБУ «УГМС по ЛНР»: 291008, ЛНР, г.о. город Луганск, г. Луганск, кв-л Аграрный, 5Б</p>
+                <p>Электронная почта: <a href='mailto:ugms@lnr.mecom.ru'>ugms@lnr.mecom.ru</a></p>
+            </div>
+            <div>
+                <p class="bold">Проезд на общественном транспорте:</p>
+                <ul class="no-bullets">
+                    <li>от автовокзала - №197, №250</li>
+                    <li>от ж\д вокзала - № 183</li>
+                    <li>с восточных кварталов – № 183, №132А</li>
+                    <li>с южных кварталов – № 141</li>
+                </ul>
+            </div>
             <div class='text-center'>
+                <h4>Остановка «Луганский государственный аграрный университет»</h4>
                 <img src="/files/imgs/map_snap.png?v=1" alt="Расположение ФГБУ «УГМС по ЛНР»" />
             </div>
-            <p class="bold"> Приемные часы: </p>
-            <p style="margin-left: 2em"> пн – чт с 09:00 до 16:00 </p>
-            <p style="margin-left: 2em"> пт с 09:00 до 15:00 </p>
-            <p style="margin-left: 2em"> перерыв с 12:00 до 12:48 </p>
-            <p style="margin-left: 2em"> 2 эт. </p>
+            <div>
+                <p class="bold"> Приемные часы: </p>
+                <ul class="no-bullets">
+                    <li> пн – чт с 09:00 до 16:00 </li>
+                    <li> пт с 09:00 до 15:00 </li>
+                    <li> перерыв с 12:00 до 12:48 </li>
+                    <li> 2 эт. </li>
+                </ul>
+            </div>
         </div>
         <? require $_SERVER['DOCUMENT_ROOT'] . '/requires/aside.php'; ?>
     </div>
